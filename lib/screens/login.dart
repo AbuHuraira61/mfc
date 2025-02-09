5ff4fc361c5a6b1a11514731f0943003781bb16c
Abu Huraira <abuhuraira1961@gmail.com>
2025-02-09 17:01:19 +0500

    initial work

---
+++ lib/screens/login.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(children: [
+      ClipPath(
+        clipper: ConcaveClipper(),
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            
+            color: Colors.red,
+            
+            ),
+          height: 350,
+
+          child: Positioned(
+            top: 100,
+            child: Image(
+              
+              filterQuality: FilterQuality.high,
+              fit: BoxFit.cover,
+              image: AssetImage('assets/food-banner.png')),
+          ),
+            
+        
+        ),
+      ),
+    ],),);
+  }
+}
+
+class ConcaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height - 50);
+    path.quadraticBezierTo(
+        size.width / 2, size.height + 30, size.width, size.height - 50);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
