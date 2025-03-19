35eaa1bb6bc77dee6f367de71d4ae6bc3b90e630
Sanan Nawaz <sanan.nawaz.897@gmail.com>
2025-03-19 13:35:18 +0500

    add managers screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mfc/Constants/colors.dart';
 import 'package:mfc/Customer%20UI/screens/splashscreen.dart';
+import 'package:mfc/Manager%20UI/ManagerPizzaScreen.dart';
 
 void main() {
   runApp(const MyApp());
