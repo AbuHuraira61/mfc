995a5153609c72b68a35b9f58a9495b0a0639799
Abu Huraira <abuhuraira1961@gmail.com>
2025-03-28 19:49:34 +0500

    aded Firebase to Burger part

--- lib/presentation/Customer UI/Home/Catagories/Burger Screen/BurgerScreen.dart
+++ lib/presentation/Customer UI/Home/Catagories/Burger Screen/BurgerScreen.dart
@@ -1,15 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:mfc/presentation/Customer%20UI/Home/Common/Singleburger_screen.dart';
+import 'package:mfc/presentation/Customer%20UI/Home/Catagories/Common/foodItemCard.dart';
 
-class BurgerScreen extends StatelessWidget {
-  final List<Map<String, dynamic>> burgers = List.generate(
-    12,
-    (index) => {
-      "name": "Burger ${index + 1}",
-      "price": 15 + index,
-      "image": "assets/beefburger.png"
-    },
-  );
+class BurgerScreen extends StatefulWidget {
+ 
+  @override
+  State<BurgerScreen> createState() => _BurgerScreenState();
+}
+
+class _BurgerScreenState extends State<BurgerScreen> {
+  Future<List<Map<String, dynamic>>> fetchBurgerData() async {
+    // ✅ Use await to wait for data before accessing .docs
+    QuerySnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore.instance
+        .collection("food_items")
+        .doc("Burger")
+        .collection("items") 
+        .get();
+
+    // ✅ Properly return the list after mapping
+    return snapshot.docs.map((doc) {
+      return {
+        "id": doc.id,
+        "name": doc["name"],
+        "image": doc["image"],
+        "prices": doc["price"],
+        "description":doc["description"],
+      };
+    }).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,116 +49,57 @@ class BurgerScreen extends StatelessWidget {
         ),
        
       ),
-      body: GridView.builder(
-        padding: EdgeInsets.all(10),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: MediaQuery.of(context).size.width > 600 ? 3 : 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          childAspectRatio: 0.8,
-        ),
-        itemCount: burgers.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => SingleBurgerScreen(),
-                ),
-              );
-            },
-            child: BurgerCard(burger: burgers[index]),
-          );
-        },
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: fetchBurgerData(),
+        builder: (context, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return Center(child: CircularProgressIndicator(),);
+          }
+          if(snapshot.hasData || snapshot.data!.isEmpty){
+            return Center(child: Text('Burgers aare not avialable'),);
+          }
+          var burgers = snapshot.data;
+
+          return GridView.builder(
+      padding: EdgeInsets.all(10),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: MediaQuery.of(context).size.width > 600 ? 3 : 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        childAspectRatio: 0.8,
       ),
+      itemCount: burgers!.length,
+      itemBuilder: (context, index) {
+        return FoodItemsCard(foodItems: burgers![index]);
+      },
+    );
+        }, )
+
+
+      // GridView.builder(
+      //   padding: EdgeInsets.all(10),
+      //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      //     crossAxisCount: MediaQuery.of(context).size.width > 600 ? 3 : 2,
+      //     crossAxisSpacing: 10,
+      //     mainAxisSpacing: 10,
+      //     childAspectRatio: 0.8,
+      //   ),
+      //   itemCount: burgers.length,
+      //   itemBuilder: (context, index) {
+      //     return GestureDetector(
+      //       onTap: () {
+      //         Navigator.push(
+      //           context,
+      //           MaterialPageRoute(
+      //             builder: (context) => SingleBurgerScreen(),
+      //           ),
+      //         );
+      //       },
+      //       child: FoodItemsCard(foodItems: burgers[index]),
+      //     );
+      //   },
+      // ),
     );
   }
 }
 
-class BurgerCard extends StatelessWidget {
-  final Map<String, dynamic> burger;
-
-  BurgerCard({required this.burger});
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Color(0xff570101),
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Center(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.asset(
-                      burger['image'],
-                      fit: BoxFit.contain,
-                      height: 150,
-                      width: 150,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 5),
-              Padding(
-                padding: const EdgeInsets.only(left: 5),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      burger['name'],
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    SizedBox(height: 5),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          '\$${burger['price']}',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Icon(
-                          Icons.shopping_cart,
-                          color: Colors.white,
-                          size: 24,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-        Positioned(
-          top: 5,
-          right: 5,
-          child: Icon(
-            Icons.favorite_border,
-            color: Colors.white,
-            size: 24,
-          ),
-        ),
-      ],
-    );
-  }
-}
