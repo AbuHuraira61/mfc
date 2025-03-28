4f70ed9db1c65f616e400029675f28d7aa69a685
SyedAliHassan512 <shamsialihassan512@gmail.com>
2025-03-28 14:55:39 +0500

    minor work

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: OrderStatusScreen(),
+      home: HomeScreen(),
     );
   }
 }
