054ca6a15e757dfe7b5fd163db33b6a767298d83
Sanan Nawaz <sanan.nawaz.897@gmail.com>
2025-04-10 12:26:06 +0500

    solve error

---
+++ lib/presentation/Manager UI/AddItem_screen.dart
@@ -0,0 +1,246 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:mfc/presentation/Manager%20UI/Extra/ManagerBurgerScreen.dart';
+
+class AddItemScreen extends StatefulWidget {
+  const AddItemScreen({super.key});
+
+  @override
+  _AddItemScreenState createState() => _AddItemScreenState();
+}
+
+class _AddItemScreenState extends State<AddItemScreen> {
+  final TextEditingController _itemNameController = TextEditingController();
+  final TextEditingController _itemPriceController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+
+  String _selectedCategory = 'Pizza';
+  File? _selectedImage;
+  final ImagePicker _picker = ImagePicker();
+
+  List<Map<String, String>> _ingredients = [
+    {"name": "", "price": ""},
+    {"name": "", "price": ""},
+    {"name": "", "price": ""}
+  ];
+
+  String? _selectedIngredient;
+
+  Future<void> _pickImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _selectedImage = File(pickedFile.path);
+      });
+    }
+  }
+
+  void _addIngredientField() {
+    setState(() {
+      _ingredients.add({"name": "", "price": ""});
+    });
+  }
+
+  void _removeIngredient(int index) {
+    setState(() {
+      if (_selectedIngredient == _ingredients[index]["name"]) {
+        _selectedIngredient = null;
+      }
+      _ingredients.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Item", style: TextStyle(fontWeight: FontWeight.bold)),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              DropdownButtonFormField<String>(
+                value: _selectedCategory,
+                items: ['Pizza', 'Burger', 'Dessert', 'Fries']
+                    .map((type) =>
+                        DropdownMenuItem(value: type, child: Text(type)))
+                    .toList(),
+                onChanged: (value) {
+                  setState(() {
+                    _selectedCategory = value!;
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: "Select Item Type",
+                  border: OutlineInputBorder(),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                ),
+              ),
+              SizedBox(height: 12),
+              TextField(
+                controller: _itemNameController,
+                decoration: InputDecoration(
+                  labelText: "Item Name",
+                  border: OutlineInputBorder(),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                ),
+              ),
+              SizedBox(height: 12),
+              TextField(
+                controller: _itemPriceController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Item Price",
+                  border: OutlineInputBorder(),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                ),
+              ),
+              SizedBox(height: 12),
+              Text("Item Image", style: TextStyle(fontSize: 16)),
+              GestureDetector(
+                onTap: _pickImage,
+                child: Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: _selectedImage != null
+                      ? ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.file(_selectedImage!,
+                              height: 150,
+                              width: double.infinity,
+                              fit: BoxFit.cover))
+                      : Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.add_a_photo,
+                                  size: 40, color: Colors.grey),
+                              SizedBox(height: 5),
+                              Text("Tap to select image",
+                                  style: TextStyle(color: Colors.grey)),
+                            ],
+                          ),
+                        ),
+                ),
+              ),
+              SizedBox(height: 12),
+              TextField(
+                controller: _descriptionController,
+                maxLines: 3,
+                decoration: InputDecoration(
+                  labelText: "Short Description",
+                  border: OutlineInputBorder(),
+                  contentPadding: EdgeInsets.all(12),
+                ),
+              ),
+              SizedBox(height: 12),
+              Text("Ingredients",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              Column(
+                children: _ingredients.asMap().entries.map((entry) {
+                  int index = entry.key;
+                  var ingredient = entry.value;
+
+                  return Row(
+                    children: [
+                      Radio<String>(
+                        value: ingredient["name"]!,
+                        groupValue: _selectedIngredient,
+                        onChanged: (value) {
+                          setState(() {
+                            _selectedIngredient = value;
+                          });
+                        },
+                      ),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Name",
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.zero,
+                          ),
+                          onChanged: (value) {
+                            setState(() {
+                              _ingredients[index]["name"] = value;
+                            });
+                          },
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      Expanded(
+                        child: TextField(
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            hintText: "Price",
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.zero,
+                          ),
+                          onChanged: (value) {
+                            setState(() {
+                              _ingredients[index]["price"] = value;
+                            });
+                          },
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.delete, color: Colors.red),
+                        onPressed: () {
+                          _removeIngredient(index);
+                        },
+                      ),
+                    ],
+                  );
+                }).toList(),
+              ),
+              TextButton.icon(
+                onPressed: _addIngredientField,
+                icon: Icon(Icons.add, color: Colors.blue),
+                label: Text("Add Ingredient",
+                    style: TextStyle(color: Colors.blue)),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xff570101),
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ManagerBurgerScreen()),
+                    );
+                  },
+                  child: Text("Add Item",
+                      style: TextStyle(color: Colors.white, fontSize: 18)),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
