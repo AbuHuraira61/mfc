7b41ceff880baae5e3e5991488aa53701209f4be
Abu Huraira <abuhuraira1961@gmail.com>
2026-02-08 16:41:39 +0500

    upgraded

--- lib/Constants/custom_snackbar.dart
+++ lib/Constants/custom_snackbar.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:mfc/Constants/colors.dart';
 
 class CustomSnackbar {
-   void showSnackbar(String title, String message) {
-    Get.snackbar(
-      title,
-      message,
-      backgroundColor: primaryColor,
-      colorText: Colors.white,
-      snackPosition: SnackPosition.BOTTOM,
-      duration: Duration(seconds: 2),
-      borderRadius: 10,
-      margin: EdgeInsets.all(10),
-      isDismissible: true,
+  static void showSnackbar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          message,
+          style: const TextStyle(color: Colors.white),
+        ),
+        backgroundColor: primaryColor,
+        duration: const Duration(seconds: 2),
+        behavior: SnackBarBehavior.floating,
+        margin: const EdgeInsets.all(10),
+      ),
     );
   }
 }
